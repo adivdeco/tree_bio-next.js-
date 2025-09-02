edcebf81a35aaf253a391faddeb5f1125726c9d9
adivdeco <sadiv120@gmail.com>
2025-09-02 17:03:46 +0530

    day 1 done (auth,shadecn)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
       hello javaus
+      <Button>click on me</Button>
     </div>
   );
 }
